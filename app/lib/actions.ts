cbba62ed97dc59ff13d698d8cd999f95d3a43a87
Sheharyar Khan <sheharyarkhan.bitsclan@gmail.com>
2024-04-03 15:35:39 +0500

    testing error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -57,6 +57,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Something went wrong');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
